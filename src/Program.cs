301b3ff90ac6039c6ddfc24b7c7de09baeec6bdf
Ankit <akumar1@ph.iitr.ac.in>
2026-04-14 23:52:16 +0530

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,23 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment the code below to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+var client = server.AcceptSocket();
+
+var stream = new NetworkStream(client);
+
+string response = "+PONG\r\n";
+byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+stream.Write(responseBytes, 0, responseBytes.Length);
+
+client.Close();
+
+
+
